552ece4b4cd36a5c0b5685aa8800577522d4f6e8
Marco Kang <kmnkit@gmail.com>
2021-04-13 17:20:28 +0900

    added

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import { Component } from "./components/component.js";
+import { VideoComponent } from "./components/page/item/video.js";
+import { TodoComponent } from "./components/page/item/todo.js";
+import { NoteComponent } from "./components/page/item/note.js";
+import { ImageComponent } from "./components/page/item/image.js";
+import { Composable, PageComponent } from "./components/page/page.js";
+
+class App {
+  private readonly page: Component & Composable;
+  constructor(appRoot: HTMLElement) {
+    this.page = new PageComponent();
+    this.page.attachTo(appRoot);
+
+    const image = new ImageComponent(
+      "Image Title",
+      "https://picsum.photos/600/300"
+    );
+    this.page.addChild(image);
+
+    const video = new VideoComponent(
+      "Video Title",
+      "https://youtu.be/zXDiOmMh6Jg"
+    );
+    this.page.addChild(video);
+
+    const note = new NoteComponent("Note Title", "Note Body");
+    this.page.addChild(note);
+
+    const todo = new TodoComponent("Todo Title", "Todo Item");
+    this.page.addChild(todo);
+  }
+}
+
+new App(document.querySelector(".document")! as HTMLElement);
